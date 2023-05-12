5843e7df376f1d8fcc1397a00d81544dda62ff51
2023-DEV1-074 <muniyappasamy.manickam@gmail.com>
2023-05-12 10:59:54 -0700

    feat: Added get all books rest api

--- src/test/java/com/bnpp/fortis/developmentbooks/controller/DevelopmentBooksControllerTest.java
+++ src/test/java/com/bnpp/fortis/developmentbooks/controller/DevelopmentBooksControllerTest.java
@@ -5,17 +5,33 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(value = DevelopmentBooksController.class)
 class DevelopmentBooksControllerTest {
+
+    private static final String GET_ALL_BOOKS_URL = "/api/developmentbooks/getallbooks";
+    @Autowired
+    MockMvc mockMvc;
+
     @Autowired
     private DevelopmentBooksController developmentBooksController;
-
 
     @Test
     @DisplayName("development book controller instance should not be null")
     void developmentControllershouldNotBeNull() {
         Assertions.assertThat(developmentBooksController).isNotNull();
+    }
+
+    @Test
+    @DisplayName(" API getBooks should return status OK")
+    void getBooksApiShouldReturnStatusOK() throws Exception {
+        mockMvc.perform(get(GET_ALL_BOOKS_URL))
+                .andExpect(status().isOk());
+
     }
 
 }
