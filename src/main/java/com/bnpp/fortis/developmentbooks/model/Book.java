19c4b6ec6e5d0b2eb0900b484e58555fa6a896db
2023-DEV1-074 <muniyappasamy.manickam@gmail.com>
2023-05-12 11:20:41 -0700

    refactor: lombok annotaions added for better readability

--- src/main/java/com/bnpp/fortis/developmentbooks/model/Book.java
+++ src/main/java/com/bnpp/fortis/developmentbooks/model/Book.java
@@ -1,38 +1,17 @@
 package com.bnpp.fortis.developmentbooks.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Book {
+
 
     private String bookTitle;
     private String author;
     private double price;
 
-    public Book(String bookTitle, String author, double price) {
-        this.bookTitle = bookTitle;
-        this.author = author;
-        this.price = price;
-    }
-
-    public String getBookTitle() {
-        return bookTitle;
-    }
-
-    public void setBookTitle(String bookTitle) {
-        this.bookTitle = bookTitle;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
 }
