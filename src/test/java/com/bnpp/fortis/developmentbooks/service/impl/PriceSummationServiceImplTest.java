384afca11145b1181635d6f8e2044968d5f38324
2023-DEV1-074 <muniyappasamy.manickam@gmail.com>
2023-05-13 08:44:43 -0700

    feat: Added price summation service, single book should return price 50

---
+++ src/test/java/com/bnpp/fortis/developmentbooks/service/impl/PriceSummationServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.bnpp.fortis.developmentbooks.service.impl;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class PriceSummationServiceImplTest {
+
+    private static final String BOOK_NAME = "Clean Code";
+    private static final double BOOK_PRICE = 50.00;
+    @Autowired
+    private PriceSummationServiceImpl priceSummationServiceImpl;
+
+    @Test
+    @DisplayName("single book  should return price 50")
+    void singleBookCalculatedPriceIsFifty() {
+
+        Double actualBookPrice = priceSummationServiceImpl.calculateBookPrice(BOOK_NAME);
+
+        assertEquals(BOOK_PRICE, actualBookPrice);
+    }
+
+
+}
