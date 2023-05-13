4c3725d00850ac9fc1d240cb48e66052c88c47da
2023-DEV1-074 <muniyappasamy.manickam@gmail.com>
2023-05-13 09:38:40 -0700

    feat: Added Discount Details Enum for 4 different kind of discounts

---
+++ src/test/java/com/bnpp/fortis/developmentbooks/storerepository/DiscountDetailsEnumTest.java
@@ -0,0 +1,16 @@
+package com.bnpp.fortis.developmentbooks.storerepository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class DiscountDetailsEnumTest {
+
+    private static final int TOTAL_NO_OF_DISCOUNT_CATEGORY = 4;
+
+    @Test
+    public void discountDetailsEnumShouldContainFourDiscountCategory() {
+
+        Assertions.assertThat(TOTAL_NO_OF_DISCOUNT_CATEGORY).isEqualTo(DiscountDetailsEnum.values().length);
+
+    }
+}
