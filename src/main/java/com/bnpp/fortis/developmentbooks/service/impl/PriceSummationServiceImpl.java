479eb83fe7866bb245b09f3f6d998c0046c3fb05
2023-DEV1-074 <muniyappasamy.manickam@gmail.com>
2023-05-13 09:42:24 -0700

    refactor: Used discount details Enum for calculating discount

--- src/main/java/com/bnpp/fortis/developmentbooks/service/impl/PriceSummationServiceImpl.java
+++ src/main/java/com/bnpp/fortis/developmentbooks/service/impl/PriceSummationServiceImpl.java
@@ -3,11 +3,13 @@ package com.bnpp.fortis.developmentbooks.service.impl;
 import com.bnpp.fortis.developmentbooks.model.BookCartDto;
 import com.bnpp.fortis.developmentbooks.service.PriceSummationService;
 import com.bnpp.fortis.developmentbooks.storerepository.BookStoreEnum;
+import com.bnpp.fortis.developmentbooks.storerepository.DiscountDetailsEnum;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -16,6 +18,8 @@ public class PriceSummationServiceImpl implements PriceSummationService {
     private static final int ZERO = 0;
     private static final int TWO = 2;
     private static final int THREE = 3;
+    private static final int ZERO_PERCENT = 0;
+
 
     private static final int FIVE = 5;
     private static final int TEN = 10;
@@ -27,17 +31,18 @@ public class PriceSummationServiceImpl implements PriceSummationService {
         Map<String, Double> bookTitlePriceMap = Arrays.stream(BookStoreEnum.values())
                 .collect(Collectors.toMap(BookStoreEnum::getBookTitle, BookStoreEnum::getPrice));
         long distinctBooks = bookCartDtoList.stream().map(BookCartDto::getName).distinct().count();
-        int discountPercentage = ZERO;
-        if (distinctBooks == TWO) {
-            discountPercentage = FIVE;
-        } else if (distinctBooks == THREE) {
-            discountPercentage = TEN;
-        }
 
         double actualPrice = bookCartDtoList.stream()
                 .mapToDouble(book -> bookTitlePriceMap.get(book.getName()) * book.getQuantity()).sum();
-        double discountedPrice = (actualPrice * discountPercentage) / HUNDRED;
+        double discountedPrice = (actualPrice * getDiscountPercentage(distinctBooks)) / HUNDRED;
 
         return (actualPrice - discountedPrice);
+    }
+
+    private int getDiscountPercentage(long numberOfDistinctBooks) {
+        
+        Optional<DiscountDetailsEnum> checkDiscount = Arrays.stream(DiscountDetailsEnum.values()).filter(discountGroup -> discountGroup.getNumberOfDistinctItems() == numberOfDistinctBooks).findFirst();
+
+        return (checkDiscount.isPresent()) ? checkDiscount.get().getDiscountPercentage() : ZERO_PERCENT;
     }
 }
