57a1a26925a34fc533294e0efa07859225862683
2023-DEV1-074 <muniyappasamy.manickam@gmail.com>
2023-05-13 09:04:41 -0700

    refactor: removed test case as it is already covered

--- src/test/java/com/bnpp/fortis/developmentbooks/service/impl/PriceSummationServiceImplTest.java
+++ src/test/java/com/bnpp/fortis/developmentbooks/service/impl/PriceSummationServiceImplTest.java
@@ -2,7 +2,6 @@ package com.bnpp.fortis.developmentbooks.service.impl;
 
 import com.bnpp.fortis.developmentbooks.model.BookCartDto;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,23 +13,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class PriceSummationServiceImplTest {
 
     private static final String BOOK_NAME = "Clean Code";
-    private static final double BOOK_PRICE = 50.00;
-
-    private static final int ONE = 1;
+  
 
     @Autowired
     private PriceSummationServiceImpl priceSummationServiceImpl;
 
-    @Test
-    @DisplayName("single book  should return price 50")
-    void singleBookCalculatedPriceIsFifty() {
-
-        BookCartDto bookDto = new BookCartDto(BOOK_NAME, ONE);
-
-        Double actualBookPrice = priceSummationServiceImpl.calculateBookPrice(bookDto);
-
-        assertEquals(BOOK_PRICE, actualBookPrice);
-    }
 
     @ParameterizedTest
     @CsvSource({"1,50", "2,100", "3,150", "4,200", "10,500"})
