aaf7bac8069c6187ec3dfe840a33aa8215af3c38
2023-DEV1-074 <muniyappasamy.manickam@gmail.com>
2023-05-14 09:47:14 -0700

    feat: Exception Handler added for invalid book name and quantity

---
+++ src/main/java/com/bnpp/fortis/developmentbooks/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.bnpp.fortis.developmentbooks.exception;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends Exception {
+
+    @ExceptionHandler({InvalidBookException.class})
+    public ResponseEntity<Object> handleBookTitleBadRequestException(InvalidBookException ex) {
+        return new ResponseEntity<Object>(ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({InvalidQuantityException.class})
+    public ResponseEntity<Object> handleBookQuantityBadRequestException(InvalidQuantityException ex) {
+        return new ResponseEntity<Object>(ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ src/test/java/com/bnpp/fortis/developmentbooks/exception/GlobalExceptionHandlerTest.java
@@ -0,0 +1,41 @@
+package com.bnpp.fortis.developmentbooks.exception;
+
+import com.fasterxml.jackson.databind.JsonMappingException;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class GlobalExceptionHandlerTest {
+    private final GlobalExceptionHandler handler = new GlobalExceptionHandler();
+
+    private static final String INVALID_BOOK = "invalid book";
+
+    private static final int INVALID_QUANTITY = -1;
+
+    @Test
+    void handleBookTitleBadRequestException() {
+        List<String> invalidBooks = new ArrayList<>();
+        invalidBooks.add(INVALID_BOOK);
+        InvalidBookException exception = new InvalidBookException(invalidBooks);
+        ResponseEntity<Object> result = handler.handleBookTitleBadRequestException(exception);
+        assertEquals(HttpStatus.BAD_REQUEST, result.getStatusCode());
+    }
+
+    @Test
+    void handleBookQuantityBadRequestException() {
+        List<Integer> invalidQuantity = new ArrayList<>();
+        invalidQuantity.add(INVALID_QUANTITY);
+        InvalidQuantityException exception = new InvalidQuantityException(invalidQuantity);
+        ResponseEntity<Object> result = handler.handleBookQuantityBadRequestException(exception);
+        assertEquals(HttpStatus.BAD_REQUEST, result.getStatusCode());
+    }
+
+
+}
